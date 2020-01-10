3dfe05c765702b0412d4748b2ef20a98f5bee5d4
Mark <mverleg.noreply@gmail.com>
2020-01-10 22:59:43 +0100

    New version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 	author_email='mdilligaf@gmail.com',
 	license='Revised BSD License (LICENSE.txt)',
 	keywords=['django', 'coding-style',],
-	version='1.8',
+	version='2.0',
 	packages=['display_exceptions'],
 	include_package_data=True,
 	zip_safe=False,
